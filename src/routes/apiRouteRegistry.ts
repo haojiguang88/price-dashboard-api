af57f7819ae7a702218b7211fa4d872eea9904f3
郝继光 <709905078@qq.com>
2026-05-28 07:25:57 +0800

    chore: 重命名生意 API 基础路由注册器

--- src/routes/apiRouteRegistry.ts
+++ src/routes/apiRouteRegistry.ts
@@ -1,10 +1,10 @@
 import type { Express } from "express";
 import { registerBusinessRoutes } from "./businessRouteRegistry";
 import { registerWorkspaceCenterRoutes } from "./workspaceCenterRouteRegistry";
-import { registerSharedRoutes } from "./sharedRouteRegistry";
+import { registerPlatformRoutes } from "./platformRouteRegistry";
 
 export const registerApiRoutes = (app: Express) => {
   registerBusinessRoutes(app);
   registerWorkspaceCenterRoutes(app);
-  registerSharedRoutes(app);
+  registerPlatformRoutes(app);
 };
--- src/routes/platformRouteRegistry.ts
+++ src/routes/platformRouteRegistry.ts
@@ -2,7 +2,7 @@ import type { Express } from "express";
 import analysisAnnotationRoutes from "./analysisAnnotationRoutes";
 import userPreferenceRoutes from "./userPreferenceRoutes";
 
-export const registerSharedRoutes = (app: Express) => {
+export const registerPlatformRoutes = (app: Express) => {
   app.use("/api", analysisAnnotationRoutes);
   app.use("/api", userPreferenceRoutes);
 };
